0532739f00faaca588491c39a0d49567f5e29207
Szymon <pasjajakub@gmail.com>
2018-07-23 21:55:46 +0200

    finish script game.sh

--- game.sh
+++ game.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+# get random number between 0 < target < 100
+declare -ir target=$(( ($RANDOM % 100) + 1 ))
+
+declare -i guess=0
+
+until (( guess == target )); do
+    read -p "Take a guess: " guess
+
+    # check if guess is a number, diffrent from zero
+    (( guess )) || continue
+
+        if (( guess < target )); then
+            echo "Highter!"
+        elif (( guess > target )); then
+            echo "Lower!"
+        else
+            echo "You found it!"
+        fi
+done
+
+exit 0
